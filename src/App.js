fd8c933e0804301b853fda4678e35f5df661b651
Habeeb <adeyinkahabeeb2010@gmail.com>
2022-07-22 00:48:22 +0000

    calculator using ReactJS

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Layout from './layout/layout';
+
+function App() {
+  return (
+    <div className="App">
+      <Layout> </Layout>
+       
+    </div>
+  );
+}
+
+export default App;
